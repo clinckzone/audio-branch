c1919c2617595dca302231897e415ac251c024f8
clinckzone <divyanshu.tiwari33@gmail.com>
2023-10-30 16:01:46 +0530

    Fix indentation issues

--- js/particle.js
+++ js/particle.js
@@ -1,63 +1,61 @@
 function Particle(x, y, bins, fft) {
-	this.x = x;
-	this.y = y;
-	// this.c = 0;
-	this.size = 15;
-	this.history = [];
-	this.bins = bins;
-	this.waveform = fft.waveform(this.bins);
-	this.level = 0;
-	this.step = 3;
-	this.direction = 0;
-	this.off = random(10);
+  this.x = x;
+  this.y = y;
+  this.size = 15;
+  this.history = [];
+  this.bins = bins;
+  this.waveform = fft.waveform(this.bins);
+  this.level = 0;
+  this.step = 3;
+  this.direction = 0;
+  this.off = random(10);
 
-	this.update = function() {
-		this.direction = map(noise(this.off), 0, 1, 0, 2*PI*noise(this.off));
-		this.x += this.step*cos(this.direction);
-		this.y += this.step*sin(this.direction);
-		this.level = mic.getLevel();
-		// this.c = map(this.level, 0, 1, 0, 360)
+  this.update = function () {
+    this.direction = map(noise(this.off), 0, 1, 0, 2 * PI * noise(this.off));
+    this.x += this.step * cos(this.direction);
+    this.y += this.step * sin(this.direction);
+    this.level = mic.getLevel();
 
-		var v = createVector(this.x, this.y);
-		this.history.push(v);
-		if(this.history.length > this.bins) { 
-			this.history.splice(0, 1);
-		}
+    var v = createVector(this.x, this.y);
+    this.history.push(v);
+    if (this.history.length > this.bins) {
+      this.history.splice(0, 1);
+    }
 
-		this.off += random(0.1*this.level);
-		this.checkEdges();
-	};
+    this.off += random(0.1 * this.level);
+    this.checkEdges();
+  };
 
-	this.checkEdges = function() {
-		if(this.x > width + this.size) {
-			this.x = 0;
-		}
-		else if(this.x < 0 - this.size) {
-			this.x = width;
-		}
-		else if(this.y > height + this.size) {
-			this.y = 0;
-		}
-		else if(this.y < 0 - this.size) {
-			this.y = height;
-		}
-	};
+  this.checkEdges = function () {
+    if (this.x > width + this.size) {
+      this.x = 0;
+    }
+    else if (this.x < 0 - this.size) {
+      this.x = width;
+    }
+    else if (this.y > height + this.size) {
+      this.y = 0;
+    }
+    else if (this.y < 0 - this.size) {
+      this.y = height;
+    }
+  };
 
-	this.show = function() {
-		// colorMode(HSB);
-		noStroke();
+  this.show = function () {
+    // colorMode(HSB);
+    noStroke();
 
-		for (var i = 0; i < this.history.length; i++) {
-			var alpha = map(i, 0, this.history.length - 1, 50, 200);
-			var size;
-			if(i < Math.floor(this.history.length/2)) {
-	 			size = map(i, 0, Math.floor(this.history.length/2), this.size*0.2, this.size);	
-			}
-			else {
-				size = map(i, Math.floor(this.history.length/2) + 1, this.history.length, this.size, this.size*0.02);			
-			}
-			fill(255, alpha);
-			ellipse(this.history[i].x, this.history[i].y, size, size);
-		}
-	};
+    for (var i = 0; i < this.history.length; i++) {
+      var alpha = map(i, 0, this.history.length - 1, 50, 200);
+      var size;
+      if (i < Math.floor(this.history.length / 2)) {
+        size = map(i, 0, Math.floor(this.history.length / 2), this.size * 0.2, this.size);
+      }
+      else {
+        size = map(i, Math.floor(this.history.length / 2) + 1, this.history.length, this.size, this.size * 0.02);
+      }
+      fill(255, alpha);
+      ellipse(this.history[i].x, this.history[i].y, size, size);
+    }
+  };
 }
--- js/script.js
+++ js/script.js
@@ -7,40 +7,29 @@ var peakDetect;
 var sound;
 var particles = [];
 
-// function preload() {
-// 	sound = loadSound('./assets/ChooLoo.mp3');
-// }
+function setup() {
+  canvas = createCanvas(window.innerWidth, window.innerHeight);
 
-function setup()	{
-	canvas = createCanvas(window.innerWidth, window.innerHeight);
-	// canvas.mousePressed(userStartAudio);
+  mic = new p5.AudioIn();
+  mic.start();
 
-	mic = new p5.AudioIn();
-	mic.start();
+  bins = pow(2, 7);
+  fft = new p5.FFT(0.5, bins);
+  fft.setInput(mic);
 
-	bins = pow(2, 7);
-	fft = new p5.FFT(0.5, bins);
-	fft.setInput(mic);
-
-	// amplitude = new p5.Amplitude(0.5);
-	// sound.amp(1);
-
-	for(var i = 0; i < 25; i++) {
-		particles.push(new Particle(random(width), random(height), bins, fft));
-	}
+  for (var i = 0; i < 25; i++) {
+    particles.push(new Particle(random(width), random(height), bins, fft));
+  }
 }
 
-function draw()		{
-	background(0);
-	for(var i = 0; i < particles.length; i++) {
-		particles[i].update();
-		particles[i].show();
-	}
-	// console.log(mic.getLevel());
+function draw() {
+  background(0);
+  for (var i = 0; i < particles.length; i++) {
+    particles[i].update();
+    particles[i].show();
+  }
 }
 
-function mousePressed()	{
-	getAudioContext().resume();
-	// if(sound.isPlaying()) sound.pause();
-	// else sound.loop();
+function mousePressed() {
+  getAudioContext().resume();
 }
